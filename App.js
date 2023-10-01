0dde8b96af8711a8970af1653b13f7e219917469
HuuPhuoc2632 <Huuphuoc.2632@gmail.com>
2023-10-01 13:30:28 +0700

    commit bai tap tuan 3

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Screen1 from './screen/Screen1';
+import Screen2 from './screen/Screen2';
+import Screen4 from './screen/Screen4';
+import Screen3 from './screen/Screen3';
+import Screen5 from './screen/Screen5';
+import Screen6 from './screen/Screen6';
+import Screen7 from './screen/Screen7';
+import Screen8 from './screen/Screen8';
+
+export default function App() {
+  return (
+    <>
+    {/* <Screen1/> */}
+    {/* <Screen2/> */}
+    {/* <Screen3/> */}
+    {/* <Screen4/> */}
+    {/* <Screen5/> */}
+    {/* <Screen6/> */}
+    {/* <Screen7/> */}
+    <Screen8/>
+    </>
+    
+  );
+}
+
+
